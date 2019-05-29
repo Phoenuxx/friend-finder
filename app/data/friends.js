aefbbec6e1ff75aa0462831c2ee9a601a0151d19
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-05-28 23:46:23 -0400

    friend maths solved, pic/name populating to modal, making some appearance adjustments and need to test for bugs/fill out friend data and add actual questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,25 +1,58 @@
 var friendArr = [
 	{
 	"name": "Sora",
-	"photo": "Kuhaku",
+	"photo": "https://via.placeholder.com/300",
 	"scores": [
 				2,
-				2,
-				2,
-				2,
+				1,
+				1,
+				1,
 				2
-			]
+			]//7
 	},
 	{
 		"name": "Bell",
-		"photo": "blank",
+		"photo": "https://via.placeholder.com/300",
 		"scores": [
+					5,
+					5,
+					5,
+					5,
+					5
+				]//25
+		},
+	{
+		"name": "three",
+		"photo": "https://via.placeholder.com/300",
+		"scores": [
+					4,
 					3,
+					1,
+					1,
+					2
+				]//11
+		},
+	{
+		"name": "four",
+		"photo": "https://via.placeholder.com/300",
+		"scores": [
+					5,
+					2,
 					3,
-					3,
-					3,
+					5,
 					3
-				]
+				]//18
+		},
+	{
+		"name": "five",
+		"photo": "https://via.placeholder.com/300",
+		"scores": [
+					5,
+					1,
+					5,
+					2,
+					3
+				]//16
 		}
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,47 +10,9 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
         friendData.push(req.body);
         // res.json(req.body);
-
-        res.json(friendData)
+        res.json(friendData);
         
-       var friendScores = [];
-        for (var i=0; i < friendData.length; i++) {
-            var totalScore = 0;
-            for (var j=0; j < friendData[i].scores.length -1; j++){
-                parsedScore = parseInt(friendData[i].scores[j]);;
-                totalScore = totalScore + parsedScore;
-                console.log("________________ friend current Total")
-                console.log(totalScore);
-            }
-            friendScores.push(totalScore);
-        }
-        var userScores = req.body.scores;
-        var userTotal = 0;
-        for (var i=0; i < userScores.length; i++) {
-            parsedUser = parseInt(userScores[i]);
-            userTotal = userTotal + parsedUser;
-            console.log("________________ user currentTotal")
-            console.log("user: " + userTotal);
-        }
-
-
-        function closest (num, arr) {
-            var curr = arr[0];
-            var diff = Math.abs (num - curr);
-            for (var val = 0; val < arr.length; val++) {
-                var newdiff = Math.abs (num - arr[val]);
-                if (newdiff < diff) {
-                    diff = newdiff;
-                    curr = arr[val];
-                }
-            }
-            return curr;
-        }
-        // array = [2, 42, 82, 122, 162, 202, 242, 282, 322, 362];
-        // number = 40;
-        console.log("----diff vv")
-        console.log(closest (userTotal, friendScores));
-
-        // console.log(friendScores);
-    });
+        });
 }
+
+
